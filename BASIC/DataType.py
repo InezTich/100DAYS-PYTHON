911a22b707d7442500d37ec5bc495f31e6b12df0
Inez Tech <74897259+InezTich@users.noreply.github.com>
2023-02-24 06:44:10 +0000

    update

--- BASIC/DataType.py
+++ BASIC/DataType.py
@@ -14,3 +14,19 @@ dict
 #Spacialize Data Type
 
 #None
+
+#Fundatmental DatType
+int and float
+
+#print(type(4 + 5))
+#print(type(3 - 2))
+#print(type(2 * 4))
+#print(type(5 / 2))
+
+#print(type(0))  #int
+#print(type(0.1)) #float
+
+print((9.9 + 1.1))
+print(2**2)
+print(3**3)
+print("H " * 3)
